994fa5389b413710ca4e80dc41ba1e9732676c9d
Ivica Šamu <ivica.samu@gmail.com>
2026-03-18 14:16:50 +0100

    Kreiran prvi entitet (Korisnici)

--- src/pages/korisnici/KorisniciPregled.jsx
+++ src/pages/korisnici/KorisniciPregled.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+import KorisniciService from "../../services/korisnici/KorisniciService";
 
 export default function KorisniciPregled(){
+
+    const[korisnici, setKorisnici] = useState([])
+
+    useEffect(()=>{
+        ucitajKorisnike()
+    },[])
+
+    async function ucitajKorisnike() {
+        await KorisniciService.get().then((odgovor)=>{
+            setKorisnici(odgovor.data)
+        })
+    }
+
     return(
-        <Container>
-            <h1>Korisnici</h1>
-            <div>Ovdje će biti tablica korisnika</div>
-        </Container>
+        <>
+        <ul>
+            {korisnici && korisnici.map((korisnik)=>(
+                <li>{korisnik.korisnickoIme}</li>
+            ))}
+        </ul>
+        </>
     )
 }
---
+++ src/services/korisnici/KorisniciService.js
@@ -0,0 +1,10 @@
+import { korisnici } from "./KorisniciPodaci"
+
+
+async function get() {
+    return {data: korisnici}
+}
+
+export default{
+    get
+}
